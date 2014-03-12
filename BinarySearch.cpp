72a906700918164b48dcf167bdd396194cefca3b
Robert Kruszewski <dzobert@gmail.com>
2014-03-12 10:37:09 +0000

    binary search

---
+++ BinarySearch.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int binarySearch(vector<int> a, int x) {
+    if(a[0] >= x || !a.size()) {
+        return 0;
+    }
+    int left = 0;
+    int right = a.size();
+    int mid;
+    while(right - left > 1) {
+        mid = floor((right + left) / 2);
+        if(a[mid] < x) {
+            left = mid;
+        } else {
+            right = mid;
+        }
+    }
+    return right;
+}
+
+int main(int argc, char * argv[]) {
+    vector<int> values = {0, 1, 3, 5, 34, 43, 63, 99};
+    int val = 43;
+    cout << "value: " << val << " found at position " << binarySearch(values, val) << endl;
+    return 0;
+}
--- BinarySearch.java
+++
@@ -1,5 +0,0 @@
-package binarysearch
-
-public class BinarySearch {
-
-}
